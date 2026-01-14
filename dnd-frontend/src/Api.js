8d8d1001ba69aaf8ee79af5ce607344301df97e4
Adeniran Ádám Kristóf <90899862+Adeniran24@users.noreply.github.com>
2026-01-14 09:08:42 +0100

    Mismatch base url

--- dnd-frontend/src/Api.js
+++ dnd-frontend/src/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://api.dnd-tool.com/api",
+    baseURL: "https://api.dnd-tool.com/api",
     headers: {
         "Content-Type": "application/json"
     }
